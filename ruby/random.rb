c6eadc0e7cb24e2bd34753e7c17ccc517b042b49
mat <134833071+k-mat438@users.noreply.github.com>
2023-10-21 11:27:28 +0900

    Update random.rb

--- ruby/random.rb
+++ ruby/random.rb
@@ -4,31 +4,13 @@ rand_num = rand(2)
 group = %w[A B C D E F]
 ans_groups = [[], []]
 
-if rand_num.zero?
-  3.times do
-    x = group.sample
-    group.delete(x)
-    ans_groups[0].push(x)
-  end
-
-  3.times do
-    x = group.sample
-    group.delete(x)
-    ans_groups[1].push(x)
-  end
-else
-  2.times do
-    x = group.sample
-    group.delete(x)
-    ans_groups[0].push(x)
-  end
-
-  4.times do
-    x = group.sample
-    group.delete(x)
-    ans_groups[1].push(x)
-  end
-end
+x = if rand_num.zero?
+      group.sample(3)
+    else
+      group.sample(2)
+    end
+ans_groups[0].push(*x)
+ans_groups[1].push(*group.difference(x))
 
 p ans_groups[0].sort
 p ans_groups[1].sort

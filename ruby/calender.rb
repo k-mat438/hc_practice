bfab5e8e1b2ccc7f4339c8d29feb38d4168a9df0
k-mat438 <a1sco00ocs1a@gmail.com>
2023-10-23 22:22:18 +0900

    modification calender.rb

--- ruby/calender.rb
+++ ruby/calender.rb
@@ -8,41 +8,35 @@ opt = OptionParser.new
 opt.on('-m VAL', Integer) { |month| option[:m] = month }
 opt.parse!(ARGV)
 
-a = Date.today
+today = Date.today
 unless option.empty?
-  if option[:m] < 1 || option[:m] > 12
-    puts "#{option[:m]} is neither a month number (1..12) nor a name"
-    exit
-  end
-  a = Date.new(a.year, option[:m], a.mday)
+  raise "#{option[:m]} is neither a month number (1..12) nor a name" if option[:m] < 1 || option[:m] > 12
+
+  today = Date.new(today.year, option[:m], today.mday)
 end
 
-puts "      #{a.mon}月 #{a.year}"
+puts "      #{today.mon}月 #{today.year}"
 puts '月 火 水 木 金 土 日'
-first_day = Date.new(a.year, a.mon, 1)
-last_day = Date.new(a.year, a.mon, -1)
 
-# 初週の空白調整
-if first_day.wday - 1 == -1
-  print ' ' * 18
-else
-  print ' ' * (first_day.wday - 1) * 3
-end
+last_day = Date.new(today.year, today.mon, -1)
 
+week = ''
 # カレンダー本体
 (1..last_day.mday).each do |i|
-  day = Date.new(a.year, a.mon, i)
-  x = day.wday - 1
-  if x == -1
-    if i < 10
-      puts " #{i}"
-    else
-      puts i
-    end
+  day = Date.new(today.year, today.mon, i)
+  wd = day.wday - 1
+  if wd == -1
+    week += if i < 10
+              " #{i}"
+            else
+              i.to_s
+            end
+    puts week.rjust(20)
+    week = ''
   elsif i < 10
-    print " #{i} "
+    week += " #{i} "
   else
-    print "#{i} "
+    week += "#{i} "
   end
 end
-puts
+puts week unless week.empty?
